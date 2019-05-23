156cef68e50f2e9ac4993e93097eb7773a99a7dc
PhilippeBui <philbui91@gmail.com>
2019-05-22 19:01:27 -0700

    Update PassGen.py

--- PassGen.py
+++ PassGen.py
@@ -7,11 +7,18 @@ import random
 #possible chars to be randomly picked for our password
 char="abcdefghijklmnopqrstuvwxyz1234567890?!@#$%"
 
+#user input for size of password
 size= input("how long would you like the password to be?")
 size= int(size)
-password = ""
 
-#iterate size times
-for x in range(size):
-  password += random.choice(char)
-print(password)
+#user input for how many passwords
+pnum = input("how many passwords?")
+pnum= int(pnum)
+
+#iterate to number of passwords needed
+for x in range(pnum):
+  password = ""
+#iterate to size of passwords
+  for x in range(size):
+    password += random.choice(char)
+  print(password)
